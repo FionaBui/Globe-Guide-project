7e58bd216e76eca110e7d27428bb51f0f795c0d0
Fiona Bui <buiphf@gmail.com>
2025-04-01 10:02:34 +0200

    add search and filter with URL synchronization

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,45 +4,96 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { CountryCard } from "../Components/Main/CountryCard";
 import { CountryFilter } from "../Components/Main/CountryFilter";
+import { useSearchParams } from "react-router-dom";
 export type  CountryType = {
   name: {common: string};
   region: string;
   cca3: string;
   flags: {png: string};
   subregion: string;
+  capital: string[];
   language?: {[key:string]:string}
 }
 function HomePage () {
-  const [countries, setCountries] = useState <CountryType[]> ([])
-  const  [regionFilter, setRegionFilter] = useState ('');
-  const [subregionFilter, setSubregionFilter]= useState('');
-
+  const [countries, setCountries] = useState <CountryType[]> ([]);
+  const [searchParams,setSearchParams]= useSearchParams()
+  const [searchInput, setSearchInput] = useState(searchParams.get('q')|| '');
+  const  [regionFilter, setRegionFilter] = useState (searchParams.get('region')||'');
+  const [subregionFilter, setSubregionFilter]= useState(searchParams.get('subregion')||'');
+  
+  
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
     .then ((response)=> response.json())
     .then ((data)=> setCountries(data))
   }, []);
+
+    const keyword = searchParams.get('q')?.toLowerCase()|| '';
     const filteredCountries = countries.filter((country) => {
+      const nameMatch = country.name.common.toLowerCase().includes(keyword);
+      const capitalMatch = country.capital?.[0]?.toLowerCase().includes(keyword)||false;
+      
+      const searchMatch = keyword ? (nameMatch || capitalMatch) : true;
       const regionMatch = regionFilter? country.region === regionFilter : true
       const subregionMatch = subregionFilter? country.subregion === subregionFilter : true
-      return regionMatch && subregionMatch
+      
+      return regionMatch && subregionMatch && searchMatch
     })
+    
+    useEffect(()=>{
+      if(searchInput.trim()===''){
+        searchParams.delete('q')
+        setSearchParams(searchParams)
+      }
+    },[searchInput, searchParams, setSearchParams]);
+    
+    const handleSearch = () => {
+      if (searchInput) {
+        searchParams.set("q", searchInput.trim());
+      } 
+      setSearchParams(searchParams);
+    };
+    
+    const handleRegionChange = (value:string) => { 
+      setRegionFilter(value);
+      if(value){
+        searchParams.set('region',value);
+        }
+        else{
+          searchParams.delete('region')
+      }
+      setSearchParams(searchParams)
+     }
+
+     const handleSubregionChange = (value:string) => {
+      setSubregionFilter(value);
+      if(value){
+        searchParams.set('subregion',value);
+      } else{
+        searchParams.delete('subregion')
+      }
+      setSearchParams(searchParams)
+     }
   return (
     <>
       <div className="homepage">
         {/* Search bar */}
         <div  className="search-container">
           <div className="search-content">
-            <input type="text" placeholder="Search by name" value="" className="search-input" />
-            <button className="btn search-btn"><FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#cf2a2a",}} /></button>
+            <input type="text" placeholder="Search..." value={searchInput} className="search-input" onChange={(e)=> setSearchInput(e.target.value)} onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleSearch();
+              }
+            }}/>
+            <button onClick={handleSearch} className="btn search-btn"><FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#cf2a2a",}} /></button>
           </div>
         </div>
         {/* Filter */}
         <CountryFilter
           region={regionFilter}
           subregion={subregionFilter}
-          onRegionChange={setRegionFilter}
-          onSubregionChange={setSubregionFilter}
+          onRegionChange={handleRegionChange}
+          onSubregionChange={handleSubregionChange}
         />
         <div className="country-list">
           {filteredCountries.length > 0 ? (
